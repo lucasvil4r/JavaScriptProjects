8af939e49a92def2af7133fa97a49e20587dd1a3
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-07-04 20:30:13 -0300

    Calculator

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,11 +1,19 @@
+const numeroCalculados = []
+
 function insert (num) {
     var numero = document.getElementById('operacao').innerHTML;
-    document.getElementById('operacao').innerHTML = numero + num;
+    document.getElementById('operacao').innerHTML = numeroCalculados;
+
+    numeroCalculados.push();
+    console.log(numeroCalculados);
 }
 function clean () {
     document.getElementById('operacao').innerHTML = "";
 }
 function back ( ) {
     var operacao = document.getElementById('operacao').innerHTML;
-    document.getElementById('operacao').innerHTML = operacao.subtring(0, operacao.length - 1);
+    document.getElementById('operacao').innerHTML = operacao.substring(0, operacao.length - 1);
+}
+
+function calculator () {
 }
